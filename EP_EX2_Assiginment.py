21e6776d43fbdd3b037bd6efe9b9a5b6048015c3
PATIPHAN09 <76670584+PATIPHAN09@users.noreply.github.com>
2023-01-05 21:36:25 +0700

    ฏ

---
+++ EP_EX2_Assiginment.py
@@ -0,0 +1,12 @@
+A   = int(input("ตัวเลขแรก : "))  
+B   = int(input("ตัวเลขที่สอง : "))
+
+# if A>B :
+#     print ("A มากกว่า B")
+# else:
+#     print ("B มากกว่า A")
+    
+if A>B :
+    print(A,  " มากกว่า " , B)
+else :
+    print(B + "มากกว่า" + A)
